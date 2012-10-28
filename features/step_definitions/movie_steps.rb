4fd2622606c60c53dfbdb5eb0b204a4a61c1f93e
sgarc22@gmail.com <sgarc22@gmail.com>
2012-10-27 17:53:55 -0700

    Got the first part working

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,11 +1,17 @@
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
+  #Movie.delete_all
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+	
+    Movie.create!(movie)
+	#new_movie.title = movie["title"]
+	#new_movie.rating = movie["rating"]
+	#new_movie.release_date = movie["release_date"]
+	#new_movie.save
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -17,6 +23,10 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   flunk "Unimplemented"
 end
 
+Then /I should see all of the movies/ do
+  flunk "Unimplemented"
+end
+
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
@@ -25,4 +35,12 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  checkAction = (uncheck) ? "uncheck" : "check"
+  ratings = rating_list.split ', '
+  ratings.each do |r|
+	steps %Q{
+		When I #{checkAction} "ratings[#{r}]"
+	}
+  end
 end
+
